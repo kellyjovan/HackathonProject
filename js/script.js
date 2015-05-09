abef8bd181e9a7f2bcb94d43af18141df1433f8c
shirokane <jcanete@aoiths.org>
2015-05-09 14:09:13 -0400

    pwerihg

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 SC.initialize({
   client_id: 'ece5c17538e1b2e30cb10d327154cff0',
-  // redirect_uri: 'REDIRECT_URL'
 });
 
 // initiate auth popup
 SC.connect(function() {
-  SC.get('/me', function(me) { 
-    alert('Hello, ' + me.username); 
+     SC.get('/tracks', { genres: 'punk'}, function(tracks) {
+    console.log(tracks);
   });
 });
